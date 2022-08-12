d34c952d2332ff8c6e1935dcf04827f7c15a13f9
samarthladani133 <samarthladani133@gmail.com>
2022-08-12 13:19:03 +0530

    Hibernate with annotaions

---
+++ src/main/java/com/HibernateAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAnnotationsApplication.class, args);
+	}
+
+}
